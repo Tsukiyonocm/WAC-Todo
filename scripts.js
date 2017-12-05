5c4269cd5bd3a9d36bc65d75e1a7dc141ab7278b
Tsukiyonocm <tsukiyonocm@yahoo.com>
2017-12-04 23:21:55 -0500

    finished version 9

--- scripts.js
+++ scripts.js
@@ -1,64 +1,41 @@
 var todoList = {
     todos: [],
-    
-    //Display Todos
-    displayTodos: function(){        
-        if (this.todos.length === 0){
-            console.log("Your TodoList is empty");
-        }
-        else {
-            console.log("My Todos: ");
-            for (var i = 0; i < this.todos.length; i++){
-                if (this.todos[i].completed === true){
-                    console.log("(x)", this.todos[i].todoText)
-                }
-                else{
-                    console.log("( )", this.todos[i].todoText)
-                }
-            }
-        }
-    },
-    
     //Add Todos
     addTodo: function(todoText){
         this.todos.push({
             todoText: todoText,
             completed: false
         });
-        this.displayTodos();
     },
-    
+
     //Changes Todos
     changeTodo: function(position, todoText){
         this.todos[position].todoText = todoText;
-        this.displayTodos();
     },
-    
+
     //Delete Todos
     deleteTodo: function(position){
-        this.deleteTodo.splice(position, 1);
-        this.displayTodos();
+        this.todos.splice(position, 1);
     },
-    
+
     //Toggle Completed
     toggleCompleted: function(position){
         var todo = this.todos[position];
         todo.completed = !todo.completed;
-        this.displayTodos();
     },
-    
+
     //Toggle All
     toggleAll: function(){
         var totalTodos = this.todos.length;
         var completedTodos = 0;
-        
+
         //Get number of completed Todos
         for (var i = 0; i < totalTodos; i++){
             if (this.todos[i].completed === true){
                 completedTodos++;
             }
         }
-        
+
         //If everything is true, make everything false.
         if (completedTodos === totalTodos){
             //Make everything false
@@ -71,19 +48,60 @@ var todoList = {
                 this.todos[i].completed = true;
             }
         }
-        this.displayTodos();
     }
 };
 
+var handlers = {
+    addTodo: function(){
+        var addTodoTextInput = document.getElementById("addTodoTextInput");
+        todoList.addTodo(addTodoTextInput.value);
+        addTodoTextInput.value = "";
+        view.displayTodos();
+    },
+    changeTodo: function(){
+        var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
+        var changeTodoTextInput = document.getElementById("changeTodoTextInput");
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = "";
+        changeTodoTextInput.value = "";
+        view.displayTodos();
+    },
+    deleteTodo: function(){
+        var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = "";
+        view.displayTodos();
+    },
+    toggleCompleted: function(){
+        var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = "";
+        view.displayTodos();
+    },
+    toggleAll: function(){
+        todoList.toggleAll();
+        view.displayTodos();
+    }
+}
 
+var view = {
+    displayTodos: function(){
+        var todosUl = document.querySelector("ul");
+        todosUl.innerHTML = "";
+        for (var i = 0; i < todoList.todos.length; i++){
+            var todoLi = document.createElement("li");
+            var todo = todoList.todos[i];
+            var todoTextWithCompletion = "";
 
-var displayTodosButton = document.getElementById("displayTodosButton");
-var toggleAllButton = document.getElementById("toggleAllButton");
+            if (todo.completed === true){
+                todoTextWithCompletion = "(x) " + todo.todoText;
+            }
+            else {
+                todoTextWithCompletion = "( ) " + todo.todoText;
+            }
 
-displayTodosButton.addEventListener('click', function(){
-    todoList.displayTodos();
-})
-
-toggleAllButton.addEventListener("click", function(){
-    todoList.toggleAll();
-})
+            todoLi.textContent = todoTextWithCompletion;
+            todosUl.appendChild(todoLi);
+        }
+    }
+}
